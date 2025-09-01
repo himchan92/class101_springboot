6f42bc33604d2cf61d6ddf3358debcc87a6b2ea0
omea92 <thdjun92@naver.com>
2025-09-01 21:06:34 +0900

    jpa 설정

--- src/main/java/com/codzero/BookRental/Book.java
+++ src/main/java/com/codzero/BookRental/Book.java
@@ -1,8 +1,28 @@
 package com.codzero.BookRental;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
 public class Book {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY) //AUTO INCREMENT
   private Long id;
   private String title;
+  private String author;
+  private LocalDateTime publishedDate;
+
+  public Book() {}
+
+  public Book(String title, String author, LocalDateTime publishedDate) {
+    this.title = title;
+    this.author = author;
+    this.publishedDate = publishedDate;
+  }
 
   public Long getId() {
     return id;
@@ -18,5 +38,21 @@ public class Book {
 
   public void setTitle(String title) {
     this.title = title;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+
+  public LocalDateTime getPublishedDate() {
+    return publishedDate;
+  }
+
+  public void setPublishedDate(LocalDateTime publishedDate) {
+    this.publishedDate = publishedDate;
   }
 }
---
+++ src/main/java/com/codzero/BookRental/BookRepository.java
@@ -0,0 +1,12 @@
+package com.codzero.BookRental;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+  List<Book> findByTitle(String title); //제목조회
+  List<Book> findByAuthor(String author);
+  List<Book> findByPublishedDate(LocalDateTime publishedDate);
+  List<Book> findByAuthorAndPublishedDate(String author, LocalDateTime publishedDate);
+}
