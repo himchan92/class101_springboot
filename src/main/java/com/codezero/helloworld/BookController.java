72075396052f7ce18cbce4ce448a8400c4113658
himchan92 <omea92@naver.com>
2025-09-01 12:58:38 +0900

    crud example

---
+++ src/main/java/com/codezero/helloworld/BookController.java
@@ -0,0 +1,54 @@
+package com.codezero.helloworld;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    private final List<Book> books = new ArrayList<>();
+
+    @GetMapping
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        book.setId((long)(books.size() + 1));
+        books.add(book);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(book);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book updateBook) {
+        for(Book book : books) {
+            if(book.getId().equals(id)) {
+                book.setTitle(updateBook.getTitle());
+                return ResponseEntity.ok(book);
+            }
+        }
+
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
+        for(Iterator<Book> iterator = books.iterator(); iterator.hasNext();) {
+            Book book = iterator.next();
+            if(book.getId().equals(id)) {
+                iterator.remove();
+                return ResponseEntity.noContent().build();
+            }
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+}
